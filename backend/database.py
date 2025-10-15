1f5e9b52d560f6e821a46d3d7d38c7e8db9864c2
Gary Yang <gy4937@princeton.edu>
2025-10-15 15:16:30 -0400

    sql tabless

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ import queue
 import psycopg2
 import dotenv
 
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 
 dotenv.load_dotenv()
 _DATABASE_URL = os.environ['DATABASE_URL']
@@ -13,20 +13,22 @@ _connection_pool = queue.Queue()
 
 def _get_connection():
     try:
-        conn = _connection_pool.get(block = False)
+        conn = _connection_pool.get(block=False)
     except queue.Empty:
         conn = psycopg2.connect(_DATABASE_URL)
     return conn
 
+
 def _put_connection(conn):
     _connection_pool.put(conn)
-    
-def execute_query(query, params = None, fetchone = False, fetchall = False, commit = False):
+
+
+def execute_query(query, params=None, fetchone=False, fetchall=False, commit=False):
     connection = _get_connection()
     try:
         with connection.cursor() as cursor:
             result = None
-            cursor.execute(query,params)
+            cursor.execute(query, params)
             if fetchone:
                 result = cursor.fetchone()
             elif fetchall:
@@ -37,10 +39,9 @@ def execute_query(query, params = None, fetchone = False, fetchall = False, comm
             return result
     finally:
         _put_connection(connection)
-    
-#-----------------------------------------------------------------------
-    
-    
+
+# -----------------------------------------------------------------------
 
 
-
+def create_user(username,email):
+    pass
