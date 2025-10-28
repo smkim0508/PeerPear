c6ef2032922788a97e92d65a3cb3d4a640672ae0
Decaylee13 <dl2635@princeton.edu>
2025-10-27 22:55:45 -0400

    revised organization page

--- public/app/organization/page.tsx
+++ public/app/organization/page.tsx
@@ -3,17 +3,32 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import EventCard from "@/components/EventCard";
+import { useRouter } from 'next/navigation';
 
 
 export default function StudentDashBoard() {
+    const router = useRouter();
+    const handleLogout = async () => {
+        try {
+            await router.push('/');
+        }
+        catch (error) {
+            console.log('Navigation error: ', error);
+        }
+    }
     return (
-        <div className="font-sans min-h-screen flex flex-col">
-            <Navbar onLoginClick={() => { }} />
-            <main className='flex-1'>
-                <section >
-
-                </section>
-
+        <div className="font-sans flex flex-col min-h-screen">
+            <Navbar userType='student' onLogoutClick={handleLogout} />
+            <main className='m-4 p-6 grid grid-cols-4 gap-2 min-h-screen'>
+                <EventCard />
+                <EventCard />
+                <EventCard />
+                <EventCard />
+                <EventCard />
+                <EventCard />
+                <EventCard />
+                <EventCard />
             </main>
             <Footer />
         </div>
