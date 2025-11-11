cb435edc71c965871ad255eafdd008000d9582a6
Sungmin Kim <sk3378@princeton.edu>
2025-11-11 15:32:29 -0500

    WIP: another test for heroku

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,14 +1,14 @@
 # main WSGI entrypoint
 import os
 import sys
-from app import create_app
 
 # NOTE: for Heroku deployment, adds current directory '/src' to system path
 CURRENT_DIR = os.path.dirname(__file__)
 if CURRENT_DIR not in sys.path:
     sys.path.append(CURRENT_DIR)
     print(f"Added {CURRENT_DIR} to sys.path")
-
+    
+from app import create_app
 app = create_app()
 
 # for local executions, with poetry
