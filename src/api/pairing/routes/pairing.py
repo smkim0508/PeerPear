d003aa9dc093cc4d11ef7818d26d1bd703fa7712
Sungmin Kim <sk3378@princeton.edu>
2025-11-12 21:04:24 -0500

    chore: cleaned up get pairing endpoint

--- src/api/pairing/routes/pairing.py
+++ src/api/pairing/routes/pairing.py
@@ -16,7 +16,7 @@ from modules.pairing.orchestrator import PairingOrchestrator
 from app_types.api.response.pairing_response import PairingResponse
 from db.crud.registration_crud import get_all_registered_users_for_event
 from common.types.event_enums import EventStatus, EventRole
-from db.crud.pairing_crud import store_new_pairing
+from db.crud.pairing_crud import store_new_pairing, get_pairings_for_event
 from common.error_response import generic_error_response
 
 # use blueprint to group routes
@@ -72,8 +72,6 @@ def pair_students_baseline():
     return jsonify(pairing_event_response.model_dump(mode="json")), 200
 
 # NOTE: temporary testing endpoint to see LLM functionality with small pairing.
-
-
 @pairing_bp.get("/test")
 def pair_students_test():
 
@@ -135,52 +133,24 @@ def get_event_pairings(event_id):
         return jsonify({"error": "event_id must be an integer"}), 400
 
     try:
-        db_session = get_db_sessionmaker()
+        client_error_msg, paired_groups = get_pairings_for_event(event_id)
+        
+        if client_error_msg:
+            return client_error_msg
 
-        with db_session as session:
-            event = session.scalar(
-                select(EventTable).where(EventTable.id == event_id))
-
-        if not event:
-            return {"error": "Event not found", "status": 404}
-
-        if not event.matches or len(event.matches) == 0:
+        if not paired_groups:
             return jsonify({"error": "No matches were found"}), 404
 
-        all_users = set()
-
-        for group in event.matches:
-            for userid in group:
-                all_users.add(userid)
-
-        users = (session.query(UserTable, EventRegistrationsTable).join(EventRegistrationsTable, EventRegistrationsTable.user_id == UserTable.id)
-                 .filter(
-            EventRegistrationsTable.event_id == event_id,
-            UserTable.id.in_(all_users)
+        response = PairingResponse(
+            event_id=event_id,
+            pairing_results=PairingResult(
+                groups=paired_groups,
+                llm_reasoning=None
+            )
         )
-            .all()
-        )
-
-        user_map = {user.id:
-                    User(
-                        id=user.id,
-                        name=f"{user.first_name} {user.last_name}",
-                        email=user.email,
-                        role=registration.role
-                    ) for user, registration in users}
-
-        paired_groups: list[PairedGroup] = []
-
-        for group in event.matches:
-            group_users = [user_map[user_id]
-                           for user_id in group if user_id in user_map]
-            paired_groups.append(PairedGroup(students=group_users))
-
-        response = PairingResponse(event_id=event_id, pairing_results=PairingResult(
-            groups=paired_groups, llm_reasoning=None))
         
         return jsonify(response.model_dump(mode="json")), 200
 
     except Exception as e:
         logger.error(f"Error getting matches of an event: {e}")
-        return jsonify({"error": "Internal server error"}), 500
+        return jsonify(generic_error_response), 500
--- src/db/crud/pairing_crud.py
+++ src/db/crud/pairing_crud.py
@@ -7,8 +7,11 @@ from app_types.api.response.event_browse_response import EventBrowseResponse, Pu
 from flask import request
 from datetime import datetime, timedelta, timezone, date
 from sqlalchemy import func
-from common.types.pairing_event import PairingResult
 from common.utils.dto_orm_conversion import dto_to_orm, orm_to_dto
+from common.types.db_status import DBStatus
+from typing import Optional
+from common.types.user import User, UserProfile
+from common.types.pairing_event import PairedGroup, PairingResult, PairingEvent
 
 def store_new_pairing(pairing_result: PairingResult, event_id: int):
     # parse the pairing result into a nested list of user ids
@@ -30,3 +33,53 @@ def store_new_pairing(pairing_result: PairingResult, event_id: int):
         event.llm_matches = groups_by_ids
         session.add(event)
         session.commit()
+
+# returns an optional error message on fail, and a pairing result
+def get_pairings_for_event(event_id: int) -> tuple[Optional[dict], Optional[list[PairedGroup]]]:
+    db_session = get_db_sessionmaker()
+
+    with db_session as session:
+        event = session.scalar(
+            select(EventTable)
+            .where(EventTable.id == event_id)
+        )
+
+        if not event:
+            return {"error": "Event not found", "status": 404}, None
+
+        if not event.matches or len(event.matches) == 0:
+            return {"error": "No matches were found", "status": 404}, None
+
+        all_users = set()
+
+        for group in event.matches:
+            for userid in group:
+                all_users.add(userid)
+
+        users = (
+            session.query(UserTable, EventRegistrationsTable)
+            .join(EventRegistrationsTable, EventRegistrationsTable.user_id == UserTable.id)
+            .filter(
+                EventRegistrationsTable.event_id == event_id,
+                UserTable.id.in_(all_users)
+            )
+            .all()
+        )
+
+        user_map = {user.id:
+            User(
+                id=user.id,
+                name=f"{user.first_name} {user.last_name}",
+                email=user.email,
+                role=registration.role
+            )
+            for user, registration in users
+        }
+
+        paired_groups: list[PairedGroup] = []
+
+        for group in event.matches:
+            group_users = [user_map[user_id] for user_id in group if user_id in user_map]
+            paired_groups.append(PairedGroup(students=group_users))
+
+        return None, paired_groups
